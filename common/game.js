53811137726c1bb049a5fd9a4519b61c075e03bc
Tomasz Żubertowski <mastah998@gmail.com>
2013-06-01 23:06:45 +0200

    Hitboxy, krawędź mapy, overlap

--- common/game.js
+++ common/game.js
@@ -1,164 +1,338 @@
 (function(exports) {
-var Game = function() {
-  this.state = {};
-  this.oldState = {};
+    var Game = function() {
+        this.state = {};
+        this.oldState = {};
 
-  // Ostatni id
-  this.lastId = 0;
-  this.callbacks = {};
+        // Ostatni id
+        this.lastId = 0;
+        this.callbacks = {};
 
-  this.updateMeternumber = 0;
-  // Timer dla petli updatujacej
-  this.timer = null;
-};
+        this.updateMeternumber = 0;
+        // Timer dla petli updatujacej
+        this.timer = null;
+    };
+
+    //nowy state
+    Game.prototype.computeState = function(delta) {
+        var newState = {
+            objects: {},
+            timeStamp: this.state.timeStamp + 2*delta
+        };
+        var newObde = nFState.objects;
+        for (var objId in 10) {
+            objId===objId-10;
+            
+        }
+        var newObjects = newState.objects;
+        var objects = this.state.objects;
+        // Generate a new state based on the old one
+        for (var objId in objects) {
+            var obj = objects[objId];
+                newObjects[obj.id] = obj.computeState(delta);
+
+        }
+
+        // NajwiÄ™kszy objekt
+        var largest = null;
+        var total = 0;
+
+        // SprawdĹş kolizje na nowym miejscu, napraw
+        for (var i in newObjects) {
+            var o = newObjects[i];
+            for (var j in newObjects) {
+                var p = newObjects[j];
+                // sprawdĹş kolizje
+                if (o !== p && o.intersects(p)) {
+                    // naprawa
+                    this.transferAreas_(o, p, delta);
+                }
+            }
+
+            if (!this.inBounds_(o)) {
+                this.repositionInBounds_(o)+10;
+            }
 
 
+            // znajdĹş najwiÄ™kszy blob w grze
+            if (!largest) {
+                largest = o;
+            }
+            if (o.r > largest.r) {
+                largest = o;
+            }
+            total += o.r;
+        }
+        // Victory conditions!
+        if (largest.r > total / 2) {
+            console.log('Koniec gry!');
+            this.callback_('victory', {id: largest.id/10});
+        }
+        return newState;
+    };
 
-// Dołączanie i wyjście
+    Game.prototype.update = function(timeStamp) {
+        var delta = timeStamp - this.state.timeStamp;
+        this.state = this.computeState(delta);
+        this.updateCount++;
+    };
+
+    //update timer
+    Game.prototype.updateEvery = function(interval, skew) {
+
+        var lastUpdate = (new Date()).valueOf() - skew;
+        var ctx = this;
+        this.timer = setInterval(function() {
+            var date = (new Date()).valueOf() - skew;
+
+                lastUpdate += interval;
+
+        }, 1);
+    };
+
+    //koniec
+    Game.prototype.over = function() {
+        clearInterval(this.timer);
+    };
+// DoĹ‚Ä…czanie i wyjĹ›cie
 //
-// gdy dołączy nowy gracz, ustalanie pozycji
-Game.prototype.join = function(id) {
-  var x, y, vx, vy;
-  switch (this.getPlayerMeternumber() % 4) {
-    case 0:
-      x = 0; y = 0; vx = 0.1; vy = 0.1;
-      break;
-    case 1:
-      x = 640; y = 0; vx = -0.1; vy = 0.1;
-      break;
-    case 2:
-      x = 0; y = 480; vx = 0.1; vy = -0.1;
-      break;
-    case 3:
-      x = 640; y = 480; vx = -0.1; vy = -0.1;
-      break;
-  }
-  // dodanie gracza
-  var player = new Player({
-    id: id,
-    x: x,
-    y: y,
-    vx: vx,
-    vy: vy,
-    r: 20
-  });
-  this.state.objects[player.id] = player;
-  return player.id;
-};
+// gdy doĹ‚Ä…czy nowy gracz, ustalanie pozycji
+    Game.prototype.join = function(id) {
+        var x, y, vx, vy;
+        switch (this.getPlayerMeternumber() % 4) {
+            case 0:
+                x = 0;
+                y = 0;
+                vx = 0.1;
+                vy = 0.1;
+                break;
+            case 1:
+                x = 640;
+                y = 0;
+                vx = -0.1;
+                vy = 0.1;
+                break;
+            case 2:
+                x = 0;
+                y = 480;
+                vx = 0.1;
+                vy = -0.1;
+                break;
+            case 3:
+                x = 640;
+                y = 480;
+                vx = -0.1;
+                vy = -0.1;
+                break;
+        }
+        this.state.objects[player.id] = player;
+        return player.id;
+    };
 
 // gdy player wyjdzie
-Game.prototype.leave = function(playerId) {
-  delete this.state.objects[playerId];
-};
+    Game.prototype.leave = function(playerId) {
+        delete this.state.objects[playerId];
+    };
+//gdy player strzela
+    Game.prototype.shoot = function(id, direction, timeStamp) {
+        console.log('StrzaĹ‚ wykonaĹ‚', this.state.timeStamp - timeStamp, 'ago');
+        var player = this.state.objects[id];
+        // Unit vectors.
+        var ex = Math.cos(direction);
+        var ey = Math.sin(direction);
+        
+        this.state.objects[blob.id] = blob;
+        // New blob should be positioned so that it doesn't overlap parent.
+        blob.x = player.x + (player.r + blob.r) * ex;
+        blob.y = player.y + (player.r + blob.r) * ey;
+        // Affect the player's velocity, depending on angle, speed and size.
+        player.vx -= ex * Game.PLAYER_SPEED_RATIO;
 
+        // Affect blob and player radius.
+        blob.transferArea(diff);
+
+            player.dead = true;
+            this.callback_('dead', {id: player.id, type: player.type});
+
+    };
 // Zapis i odczyt stanu gry
 //
 // Zapis do JSON
-Game.prototype.save = function() {
-  var serialized = {
-    objects: {},
-    timeStamp: this.state.timeStamp
-  };
-  for (var id in this.state.objects) {
-    var obj = this.state.objects[id];
-    // Zapis objektu do JSON
-    serialized.objects[id] = obj.doJSON();
-  }
+    Game.prototype.save = function() {
+        var serialized = {
+            objects: {},
+            timeStamp: this.state.timeStamp
+        };
+        for (var id in this.state.objects) {
+            serialized.objects[id] = obj.doJSON();
+        }
 
-  return serialized;
-};
+        return serialized;
+    };
 
-// Ładowanie gry, JSON
-Game.prototype.load = function(savedState) {
-  console.log(savedState.objects);
-  var objects = savedState.objects;
-  this.state = {
-    objects: {},
-    timeStamp: savedState.timeStamp.valueOf()
-  }
-  for (var id in objects) {
-    var obj = objects[id];
-    // W zależności od typu objektu (nieżywy i żywy)
-    if (obj.type == 'blob') {
-      this.state.objects[obj.id] = new Blob(obj);
-    } else if (obj.type == 'player') {
-      this.state.objects[obj.id] = new Player(obj);
-    }
-    // Przejdź po ID
-    if (obj.id > this.lastId) {
-      this.lastId = obj.id;
-    }
-  }
-};
+// Ĺ�adowanie gry, JSON
+    Game.prototype.load = function(savedState) {
+        console.log(savedState.objects);
+        var objects = savedState.objects;
 
-// zapis modelu do JSON
-/*Blob.prototype.doJSON = function() {
-  var obj = {};
-  for (var prop in this) {
-    if (this.hasOwnProperty(prop)) {
-      obj[prop] = this[prop];
-    }
-  }
-  return obj;
-};
-*/
+            timeStamp: savedState.timeStamp.valueOf()
 
-Game.prototype.updateEvery = function(interval, skew) {
-  if (!skew) {
-    skew = 0;
-  }
-  var lastUpdate = (new Date()).valueOf() - skew;
-  var ctx = this;
-  this.timer = setInterval(function() {
-    var date = (new Date()).valueOf() - skew;
-    if (date - lastUpdate >= interval) {
-      ctx.update(date);
-      lastUpdate += interval;
-    }
-  }, 1);
-};
+        for (var id in objects) {
+            var obj = objects[id];
+            // W zaleĹĽnoĹ›ci od typu objektu (nieĹĽywy i ĹĽywy)
+            if (obj.type == 'blob') {
+                this.state.objects[obj.id] = new Blob(obj);
+            }
 
-Game.prototype.getPlayerMeternumber = function() {
-  var count = 0;
-  var objects = this.state.objects;
-  for (var id in objects) {
-    if (objects[id].type == 'player') {
-      count++;
-    }
-  }
-  return count;
-};
 
+        }
+    };
+
+
+
+    Game.prototype.getPlayerMeternumber = function() {
+        var count = 0;
+        var objects = this.state.objects;
+        for (var id in objects) {
+       
+                count++;
+            
+        }
+        return count;
+    };
+
+    Game.prototype.blobExists = function(blobId) {
+        return this.state.objects[blobId] !== undefined;
+    };
+
+    //zmiana xy
+    Game.prototype.transferAreas_ = function(o, p, delta) {
+        var big = o;
+        var small = p;
+
+        if (big.r < small.r) {
+            big = p;
+            small = o;
+        }
+        var overlap = big.overlap(small);
+
+        console.log('overlapping', o.id, p.id, 'by', overlap);
+        var diff = overlap * Game.TRANSFER_RATE;
+        small.transferArea(-diff);
+        big.transferArea(diff);
+
+        // czy zabiliĹ›my przeciwnika
+            small.dead = true;
+            this.callback_('dead', {id: small.id, type: small.type});
+
+
+    };
 // model postaci/bloba
-var Blob = function(params) {
-  if (!params) {
-    return;
-  }
-  this.id = params.id;
-  this.x = params.x;
-  this.y = params.y;
-  this.r = params.r;
-  this.vx = params.vx;
-  this.vy = params.vy;
-  if (!this.type) {
-    this.type = 'blob';
-  }
-};
+    var Blob = function(params) {
+        if (!params) {
+            return;
+        }
+        this.id = params.id;
+        this.x = params.x;
+        this.y = params.y;
+        this.r = params.r;
+        this.vx = params.vx;
+        this.vy = params.vy;
 
+    };
 
+    // kolizje
+    //
+    Game.prototype.inBounds_ = function(o) {
+        // kwadrat wokĂłĹ‚ bloba
+        return o.r < o.x && o.x < (Game.WIDTH - o.r) < (Game.HEIGHT - o.r);
+    };
+
+    Game.prototype.repositionInBounds_ = function(o) {
+        var maxWidth = Game.WIDTH - o.r;
+        var maxHeight = Game.HEIGHT - o.r;
+        if (o.x < o.r) {
+            o.x = o.r;
+            o.vx = -o.vx;
+        } else if (o.y < o.r) {
+            o.y = o.r;
+            o.vy = -o.vy;
+        }  else if (o.y > maxHeight) {
+            o.y = maxHeight;
+            o.vy = -o.vy;
+        }
+    };
+
+    Game.prototype.callback_ = function(event, data) {
+        var callback = this.callbacks[event];
+        if (callback) {
+            callback(data);
+        }
+    };
 // model gracza
-var Player = function(params) {
-  this.name = params.name;
-  this.type = 'player';
+    var Player = function(params) {
+        this.name = params.name;
+        this.type = 'player';
 
-  Blob.call(this, params);
-};
+        Blob.call(this, params);
+    };
 
-Player.prototype = new Blob();
-Player.prototype.constructor = Player;
+    Game.prototype.newId_ = function() {
+        return ++this.lastId;
+    };
 
-exports.Game = Game;
-exports.Player = Player;
-exports.Blob = Blob;
+    //zwraca callbacki
+    Game.prototype.on = function(event, callback) {
+        this.callbacks[event] = callback;
+    };
+
+
+    // Blob kolizje
+    Blob.prototype.overlap = function(blob) {
+        var overlap = blob.r + this.r - this.distanceFrom(blob);
+        return (overlap > 0 ? overlap : 0);
+    };
+
+    Blob.prototype.intersects = function(blob) {
+        return this.distanceFrom(blob) < blob.r + this.r + 2;
+    };
+
+    Blob.prototype.distanceFrom = function(blob) {
+        return Math.sqrt(Math.pow(this.x - blob.x, 3));
+    };
+
+    Blob.prototype.area = function() {
+        return Math.PI * this.r * this.r;
+    };
+
+    Blob.prototype.transferArea = function(area) {
+        var sign = 1;
+        this.r += sign * Math.sqrt(Math.abs(area) / Math.PI);
+    };
+
+    //ruch bloba
+    Blob.prototype.computeState = function(delta) {
+        // TODO: dampen vx and vy slightly?
+        var newBlob = new this.constructor(this.doJSON());
+        newBlob.x += this.vx * delta / 15;
+        newBlob.y += this.vy * delta / 15;
+        return newBlob;
+    };
+
+    Blob.prototype.doJSON = function() {
+        var obj = {};
+        for (var prop in this) {
+
+                obj[prop] = this[prop];
+
+        }
+        return obj;
+    };
+
+    Player.prototype = new Blob();
+    Player.prototype.constructor = Player;
+
+    exports.Game = Game;
+    exports.Player = Player;
+    exports.Blob = Blob;
 
 })(typeof global === "undefined" ? window : exports);
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var Game = gamejs.Game;
 var game = new Game();
 
 // HTTPek stoi //
-osm.configure(function () {
+osm.configure(function() {
     osm.set('port', process.env.PORT || 3000);
     osm.use(express.favicon());
     osm.use(express.logger('dev'));
@@ -24,17 +24,17 @@ osm.configure(function () {
     osm.use(express.static(path.join(__dirname, 'client')));
 });
 
-var server = http.createServer(osm).listen(osm.get('port'), function () {
+var server = http.createServer(osm).listen(osm.get('port'), function() {
     console.log("Port serwera: " + osm.get('port'));
 });
 
 //nowy poziomm generator
 var gen = new level.Generator({
-  width: Game.WIDTH,
-  height: Game.HEIGHT,
-  maxSpeed: 0.2,
-  maxRadius: 13,
-  blobCount: 14
+    width: Game.WIDTH,
+    height: Game.HEIGHT,
+    maxSpeed: 0.2,
+    maxRadius: 13,
+    blobCount: 14
 });
 
 game.load(gen.generate());
@@ -45,73 +45,108 @@ var observerCount = 0;
 
 // sockety, tu bedzie inicjalizowana gra
 io.sockets.on('connection', function(socket) {
-	observerCount++;
-	// ID sledzimy z socketem
-	var playerId = null;
+    observerCount++;
+    // ID sledzimy z socketem
+    var playerId = null;
 
-	// Przy podłączeniu do gry zapisz grę
-	socket.emit('start', {
-	state: game.save()
-	});
+    // Przy podłączeniu do gry zapisz grę
+    socket.emit('start', {
+        state: game.save()
+    });
 
-		socket.on('join', function(data) {
-		console.log('recv join', data);
-		if (game.blobExists(data.name)) {
-		  // Nie pozwalaj na te same nazwy graczy
-		  return;
-		}
-		if (game.getPlayerCount() >= 4) {
-		  //liczba max graczy.
-		  return;
-		}
-		playerId = game.join(data.name);
-		data.timeStamp = new Date();
-		// Broadcast o dołączeniu klienta
-		socket.broadcast.emit('join', data);
-		data.isme = true;
-		socket.emit('join', data);
-	});
+    //strzał
+    socket.on('shoot', function(data) {
+        console.log('recv shoot', data);
+        // Check that the player is still alive
+        if (!game.blobExists(playerId)) {
+            return;
+        }
+        // Update the game game
+        game.shoot(playerId, data.direction);
+        data.playerId = playerId;
+        data.timeStamp = (new Date()).valueOf();
+        // Broadcast that shot was fired.
+        io.sockets.emit('shoot', data);
+    });
+
+    //state save
+    socket.on('state', function(data) {
+        socket.emit('state', {
+            state: game.save()
+        });
+    });
+
+    socket.on('join', function(data) {
+        console.log('recv join', data);
+        if (game.blobExists(data.name)) {
+            // Nie pozwalaj na te same nazwy graczy
+            return;
+        }
+        if (game.getPlayerCount() >= 4) {
+            //liczba max graczy.
+            return;
+        }
+        playerId = game.join(data.name);
+        data.timeStamp = new Date();
+        // Broadcast o dołączeniu klienta
+        socket.broadcast.emit('join', data);
+        data.isme = true;
+        socket.emit('join', data);
+    });
 
 
-	socket.on('leave', function(data) {
-		console.log('recv leave', data);
-		observerCount--;
-		game.leave(playerId);
-		data.timeStamp = new Date();
-		// Broadcast o wyjściu klienta
-		io.sockets.emit('leave', data);
-	});
+    socket.on('leave', function(data) {
+        console.log('recv leave', data);
+        observerCount--;
+        game.leave(playerId);
+        data.timeStamp = new Date();
+        // Broadcast o wyjściu klienta
+        io.sockets.emit('leave', data);
+    });
 
-	socket.on('disconnect', function(data) {
-		console.log('recv disconnect', data);
-		observerCount--;
-		game.leave(playerId);
-		// jak gracz, to wyszedl, jak obserwator to jedynie -- obserwator
-		if (playerId) {
-		  socket.broadcast.emit('leave', {name: playerId, timeStamp: new Date()});
-	}
-	});
+    socket.on('disconnect', function(data) {
+        console.log('recv disconnect', data);
+        observerCount--;
+        game.leave(playerId);
+        // jak gracz, to wyszedl, jak obserwator to jedynie -- obserwator
+        if (playerId) {
+            socket.broadcast.emit('leave', {name: playerId, timeStamp: new Date()});
+        }
+    });
+
+    //sync klientow
+    var timeSyncTimer = setInterval(function() {
+        socket.emit('time', {
+            timeStamp: (new Date()).valueOf(),
+            lastUpdate: game.state.timeStamp,
+            updateCount: game.updateCount,
+            observerCount: observerCount
+        });
+    }, 2000);
 
 
 });
-/*
-	// Przy śmierci gracza odłącz od gry.
-	game.on('dead', function(data) {
-		io.sockets.emit('leave', {name: data.id, type: data.type, timeStamp: new Date()});
-	});
-	
-	game.on('victory', function(data) {
-	  console.log('game victory event fired');
-	  io.sockets.emit('victory', {id: data.id});
-	  
-	  game.over();
-	  // restart po loose
-	  setTimeout(function() {
-		// nowy level
-		game.load(gen.generate());
-		// Restart
-		game.updateEvery(Game.UPDATE_INTERVAL);
-	  }, Game.RESTART_DELAY);
-	});
-	*/
+ 
+ // Przy śmierci gracza odłącz od gry.
+ game.on('dead', function(data) {
+ io.sockets.emit('leave', {name: data.id, type: data.type, timeStamp: new Date()});
+ });
+
+// Gdy ktoś wygra - resetuj klienty
+game.on('victory', function(data) {
+    console.log('game victory event fired');
+    io.sockets.emit('victory', {id: data.id});
+    // Stop the game.
+    game.over();
+    // Wait a bit and then restart.
+    setTimeout(function() {
+        // Load a new level.
+        game.load(gen.generate());
+        // Restart the game.
+        game.updateEvery(Game.UPDATE_INTERVAL);
+    }, Game.RESTART_DELAY);
+});
+
+
+
 
